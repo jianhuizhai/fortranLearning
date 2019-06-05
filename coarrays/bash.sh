a4e0cd7c71f19c706b268b02339561704eea8b33
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-05 23:10:46 +0200

    Delete bash.sh

--- coarrays/bash.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3401.f90 -o 3401 -lcaf_mpi
-caf ch3401.f90 -o 3401_caf
-#gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3402.f90 -o 3402
-#gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3403.f90 -o 3403
-#gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3404.f90 -o 3404

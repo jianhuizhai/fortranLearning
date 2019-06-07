33f4d230a43b0699f9f82aecc594ce0c73c95381
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-07 21:52:15 +0200

    Add files via upload

--- coarrays/compile.sh
+++ coarrays/compile.sh
@@ -2,6 +2,6 @@
 
 gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3401.f90 -o 3401 -lcaf_mpi
 caf ch3401.f90 -o 3401_caf
-#gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3402.f90 -o 3402
-#gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3403.f90 -o 3403
-#gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3404.f90 -o 3404
+gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3402.f90 -o 3402 -lcaf_mpi
+gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3403.f90 -o 3403 -lcaf_mpi
+gfortran -fcoarray=lib -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch3404.f90 -o 3404 -lcaf_mpi
--- coarrays/run.sh
+++ coarrays/run.sh
@@ -1,4 +1,8 @@
 #! /usr/bin
 
-cafrun -np 4 ./3401
+cafrun -np 8 ./3401
 cafrun -np 4 ./3401_caf
+cafrun -np 4 ./3402
+cafrun -np 4 ./3403
+cafrun -np 4 ./3404
+

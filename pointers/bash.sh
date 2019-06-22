52f77ac2af76a3c508f2eeca346e4eeb27bc082e
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-22 23:06:27 +0200

    Add files via upload

---
+++ pointers/bash.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1801.f90 -o 1801
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1802.f90 -o 1802
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1803.f90 -o 1803
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1804.f90 -o 1804
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1805.f90 -o 1805
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1806.f90 -o 1806
+gfortran -std=f2008 -fcheck=all -fall-intrinsics -ffpe-trap=zero,overflow,underflow -Wall ch1807.f90 -o 1807

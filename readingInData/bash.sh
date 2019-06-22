1b880b1d3eee4b3618a5c34deebefd18e3790ee3
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-22 23:07:34 +0200

    Add files via upload

---
+++ readingInData/bash.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1001.f90 -o 1001
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1002.f90 -o 1002
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1003.f90 -o 1003
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1004.f90 -o 1004
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1005.f90 -o 1005
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1006.f90 -o 1006
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1007.f90 -o 1007
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1008.f90 -o 1008
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch1009.f90 -o 1009

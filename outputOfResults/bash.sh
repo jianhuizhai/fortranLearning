6029d4e661f722bb318ba162fa2bfa7dac38a211
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-22 23:10:15 +0200

    Add files via upload

---
+++ outputOfResults/bash.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0901.f90 -o 0901
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0902.f90 -o 0902
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0903.f90 -o 0903
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0904.f90 -o 0904
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0905.f90 -o 0905
+gfortran -std=f2008 -fcheck=all -Wall ch0906.f90 -o 0906
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0907.f90 -o 0907
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0908.f90 -o 0908
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0909.f90 -o 0909
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0910.f90 -o 0910
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0911.f90 -o 0911
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0912.f90 -o 0912
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0913.f90 -o 0913
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0914.f90 -o 0914
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0915.f90 -o 0915
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0916.f90 -o 0916
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0917.f90 -o 0917
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0918.f90 -o 0918
+gfortran -std=f2008 -fcheck=all -ffpe-trap=zero,overflow,underflow -Wall ch0919.f90 -o 0919

b18baffdbcdd922927bc2a29d4e6ca1534698eba
jianhuizhai <jianhuizhai89@gmail.com>
2019-06-24 22:32:13 +0200

    Add files via upload

---
+++ wholeArray/bash.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+gfortran -std=f2008 -fcheck=all -Wall ch0801.f90 -o 0801
+gfortran -std=f2008 -fcheck=all -Wall ch0802.f90 -o 0802
+gfortran -std=f2008 -fcheck=all -Wall ch0803.f90 -o 0803
+gfortran -std=f2008 -fcheck=all -Wall ch0804.f90 -o 0804
+gfortran -std=f2008 -fcheck=all -Wall ch0805.f90 -o 0805
+gfortran -std=f2008 -fcheck=all -Wall ch0806.f90 -o 0806
+gfortran -std=f2008 -fcheck=all -Wall ch0807.f90 -o 0807
+gfortran -std=f2008 -fcheck=all -Wall ch0808.f90 -o 0808
+gfortran -std=f2008 -fcheck=all -Wall ch0809.f90 -o 0809
+gfortran -std=f2008 -fcheck=all -Wall ch0810.f90 -o 0810
+gfortran -std=f2008 -fcheck=all -Wall ch0811.f90 -o 0811
+gfortran -std=f2008 -fcheck=all -Wall ch0812.f90 -o 0812
+gfortran -std=f2008 -fcheck=all -Wall ch0813.f90 -o 0813
